46c2f95558c9e8176c7081bd6e6e78cd51b38573
Ludwig32 <ludwigleandro150@gmail.com>
2024-11-26 14:45:12 -0300

    Sql_pronto

--- sa_felipe (1).sql
+++ sa_felipe (1).sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 11/11/2024 às 01:27
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
+-- Tempo de geração: 26-Nov-2024 às 18:33
+-- Versão do servidor: 10.4.22-MariaDB
+-- versão do PHP: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,16 +24,16 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `categoria`
+-- Estrutura da tabela `categoria`
 --
 
 CREATE TABLE `categoria` (
   `id_categ` int(11) NOT NULL,
   `desc_categ` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Despejando dados para a tabela `categoria`
+-- Extraindo dados da tabela `categoria`
 --
 
 INSERT INTO `categoria` (`id_categ`, `desc_categ`) VALUES
@@ -49,7 +49,27 @@ INSERT INTO `categoria` (`id_categ`, `desc_categ`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `login_usuarios`
+-- Estrutura da tabela `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id_cliente` int(11) NOT NULL,
+  `nome_cliente` varchar(50) NOT NULL,
+  `email_cliente` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `cliente`
+--
+
+INSERT INTO `cliente` (`id_cliente`, `nome_cliente`, `email_cliente`) VALUES
+(1, 'felipe', 'felipe@gmail.com'),
+(3, 'leandro', 'lendro@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `login_usuarios`
 --
 
 CREATE TABLE `login_usuarios` (
@@ -58,10 +78,10 @@ CREATE TABLE `login_usuarios` (
   `senha` varchar(30) NOT NULL,
   `nivel` varchar(18) DEFAULT NULL,
   `funcionario` varchar(30) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Despejando dados para a tabela `login_usuarios`
+-- Extraindo dados da tabela `login_usuarios`
 --
 
 INSERT INTO `login_usuarios` (`id_login`, `usuario`, `senha`, `nivel`, `funcionario`) VALUES
@@ -72,7 +92,7 @@ INSERT INTO `login_usuarios` (`id_login`, `usuario`, `senha`, `nivel`, `funciona
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `os`
+-- Estrutura da tabela `os`
 --
 
 CREATE TABLE `os` (
@@ -86,24 +106,21 @@ CREATE TABLE `os` (
   `os_preco` decimal(10,0) NOT NULL,
   `os_prod_1` varchar(50) NOT NULL,
   `os_prod_2` varchar(50) DEFAULT NULL,
-  `os_prod_3` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `os_prod_3` varchar(50) DEFAULT NULL,
+  `cliente` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Despejando dados para a tabela `os`
+-- Extraindo dados da tabela `os`
 --
 
-INSERT INTO `os` (`id_os`, `os_data_inicio`, `os_data_fim`, `os_previsto`, `os_descriçao`, `os_atribuido`, `os_sit`, `os_preco`, `os_prod_1`, `os_prod_2`, `os_prod_3`) VALUES
-(1, '2024-11-01', '2024-11-16', '2024-11-29', 'trocar placa mãe e fazer limpeza do gabinete.', '3', 'atrasado', 0, '', NULL, NULL),
-(7, '2024-11-09', NULL, '2024-11-14', 'mama', 'testante', '', 6924, '1', '1', NULL),
-(8, '2024-11-09', NULL, '2024-11-12', 'a', 'Jerso', '', 1, '6', '6', NULL),
-(9, '2024-11-09', NULL, '2024-11-11', 'a', 'testante', '', 1, '1', '6', NULL),
-(10, '2024-11-09', NULL, '2024-11-20', '123', 'testante', 'Em Andamento', 123, '5', '6', NULL);
+INSERT INTO `os` (`id_os`, `os_data_inicio`, `os_data_fim`, `os_previsto`, `os_descriçao`, `os_atribuido`, `os_sit`, `os_preco`, `os_prod_1`, `os_prod_2`, `os_prod_3`, `cliente`) VALUES
+(12, '2024-11-25', NULL, '2024-11-15', 'leandro estragou o pc', 'Jerso', 'Pendente', '1000', '3090 nvi', 'LG 144hz', NULL, 'leandro');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `produtos`
+-- Estrutura da tabela `produtos`
 --
 
 CREATE TABLE `produtos` (
@@ -112,58 +129,70 @@ CREATE TABLE `produtos` (
   `qntd_prod` int(11) DEFAULT NULL,
   `prec_prod` decimal(10,2) DEFAULT NULL,
   `categoria` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Despejando dados para a tabela `produtos`
+-- Extraindo dados da tabela `produtos`
 --
 
 INSERT INTO `produtos` (`id_prod`, `desc_prod`, `qntd_prod`, `prec_prod`, `categoria`) VALUES
-(1, 'RTX', 3, 2000.00, 'placa de video'),
-(2, 'ryzen 5 rtx on sla oq', 123, 2500.00, 'placa'),
-(3, 'LG 144hz', 4, 200.00, 'Monitor'),
-(4, 'BackLit Preto', 2, 300.00, 'Teclado'),
-(5, 'teclado redragon', 123, 300.00, 'Teclado'),
-(6, '3090 nvidea', 123, 3500.99, 'placa de video');
+(1, 'RTX', 3, '2000.00', 'placa de video'),
+(2, 'ryzen 5 rtx on sla oq', 123, '2500.00', 'placa'),
+(3, 'LG 144hz', 3, '200.00', 'Monitor'),
+(4, 'BackLit Preto', 2, '300.00', 'Teclado'),
+(5, 'teclado redragon', 123, '300.00', 'Teclado'),
+(6, '3090 nvi', 122, '3500.99', 'Mouse');
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices de tabela `categoria`
+-- Índices para tabela `categoria`
 --
 ALTER TABLE `categoria`
   ADD PRIMARY KEY (`id_categ`);
 
 --
--- Índices de tabela `login_usuarios`
+-- Índices para tabela `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id_cliente`);
+
+--
+-- Índices para tabela `login_usuarios`
 --
 ALTER TABLE `login_usuarios`
   ADD PRIMARY KEY (`id_login`);
 
 --
--- Índices de tabela `os`
+-- Índices para tabela `os`
 --
 ALTER TABLE `os`
   ADD PRIMARY KEY (`id_os`),
   ADD KEY `fk_cod_login` (`os_atribuido`);
 
 --
--- Índices de tabela `produtos`
+-- Índices para tabela `produtos`
 --
 ALTER TABLE `produtos`
   ADD PRIMARY KEY (`id_prod`);
 
 --
--- AUTO_INCREMENT para tabelas despejadas
+-- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
 -- AUTO_INCREMENT de tabela `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `id_categ` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id_categ` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de tabela `login_usuarios`
@@ -175,13 +204,13 @@ ALTER TABLE `login_usuarios`
 -- AUTO_INCREMENT de tabela `os`
 --
 ALTER TABLE `os`
-  MODIFY `id_os` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_os` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de tabela `produtos`
 --
 ALTER TABLE `produtos`
-  MODIFY `id_prod` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_prod` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
